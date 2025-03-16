785286151a807cf1562d18330969d75f8659dbc3
kimgeunhoo <127118278+kimgeunhoo@users.noreply.github.com>
2025-03-16 22:27:54 +0900

    2025-03-16 Test

--- PlayerInput/main.c
+++ PlayerInput/main.c
@@ -1,15 +1,14 @@
 // 무엇을 만들 것인가?
 #include "playerInput.h"
 
+
 int main() {
-	int playerX = 3;
-	int playerY = 3;
 	
 	while (1)  // 프로그램 계속 실행 시키기
 	{
-		setCursorVisible(false);
-		setCursorPos(playerX, playerY);
-		printf("♥");
+		int playerX = 3;
+		int playerY = 3;
+
 		GetInput(&playerX, &playerY);
 		
 	}
--- PlayerInput/playerInput.c
+++ PlayerInput/playerInput.c
@@ -1,8 +1,16 @@
 #include "playerInput.h"
 
 void GetInput(int* playerX, int* playerY) {
+
+	setCursorPos(&playerX, &playerY);
+	printf("♥");
+
 	if (_kbhit())
 	{
+		setCursorVisible(false);
+		setCursorPos(&playerX, &playerY);
+		printf("  ");
+
 		if (GetAsyncKeyState(VK_LEFT))  // 왼쪽 화살표 입력 시
 		{
 			playerX -= 2;
@@ -19,7 +27,15 @@ void GetInput(int* playerX, int* playerY) {
 		{
 			playerY += 1;
 		}
+		else 
+		{
+
+		}
+		
 	}
+	setCursorPos(&playerX, &playerY);
+	printf("♥");
+	Sleep(50);
 }
 
 void setCursorPos(int x, int y)
