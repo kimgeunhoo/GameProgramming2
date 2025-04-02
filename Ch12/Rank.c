4c34650cdbb7ff2cee5175dbf5b27181e594e034
kimgeunhoo <127118278+kimgeunhoo@users.noreply.github.com>
2025-04-02 17:49:37 +0900

    (2025-04-02) 랭킹 시스템 구현완료

--- Ch12/Rank.c
+++ Ch12/Rank.c
@@ -10,10 +10,10 @@ void FileSaveTemp()
 	//fputc('a', fp);
 	//fputc('a', fp);
 	// fputs
-	fputs("a\naa", fp);
+	//fputs("aaa", fp);
 
 	// fprintf
-	fprintf(fp, "%d %s %d", 1, "Alice", 100);
+	fprintf(fp, "%d %d %s", 1, 100, "Alice");
 	fclose(fp);
 }
 
@@ -32,10 +32,9 @@ void FileLoadTemp()
 {
 	FILE* fp2 = fopen("Text.txt", "r");
 
-	char mstring[12];
-	fgets(mstring ,12, fp2);
-
-	printf("%s\n", mstring);
+	//char mstring[12];
+	//fgets(mstring ,12, fp2);
+	//printf("%s\n", mstring);
 
 	int order = 0;
 	int score = 0;
@@ -60,8 +59,12 @@ int LoadRank(const char* fileName, Rank rank[])
 		return;
 	}
 
-	while (fscanf(fptr, "%d %d %49s", &rank[count].order, &rank[count].score, rank[count].name) != EOF) // 파일이 끝날 때 까지 읽어오기
+	while (1) // 파일이 끝날 때 까지 읽어오기
 	{
+		if (fscanf(fptr, "%d %d %49s", &rank[count].order, &rank[count].score, rank[count].name) == EOF) 
+		{
+			break;
+		}
 		count++;
 	}
 
@@ -84,7 +87,7 @@ void PrintRanking(Rank rank[], int count)
 
 	for (int i = 0; i < count; i++)
 	{
-		printf("| %4d | %4d | %-16s |\n", rank[i].order, rank[i].score, rank[i].name);
+		printf("| %4d | %4d | %-16s |\n", i + 1, rank[i].score, rank[i].name);
 	}
 
 	printf("+------+------+------------------+\n");
@@ -104,4 +107,61 @@ void AddRank(Rank rank[], int* order/*score라고 생각*/, const char* name, int sc
 		printf(" !최대 플레이어 저장 수를 초과하였습니다!\n");
 	}
 	
+}
+
+void AddRankData(Rank rank[], int* order)
+{
+	if (*order < MAXPLAYER) { // 값 호출해야 한다면 역참조* 주소참조는 &, 
+		printf("새로운 플레이어의 이름을 입력하세요 : ");
+		char newName[MAXLENGTH];
+		int newScore;
+
+		scanf("%49s", newName);
+		printf("점수를 입력하세요 : ");
+		scanf("%d", &newScore);
+
+		AddRank(rank, order, newName, newScore); // 이미 order 가 주소이기에 주소값 &를 안쓴다
+	}
+}
+
+void DeleteRankData(Rank rank[], int* order, int index)
+{
+	// 1. order 배열의 순서 , -1 해줘야 한다 (0부터 시작)
+	// 2. 지우고 싶은 순서를 전달해 줘야 한다. order이다.
+	//
+
+	if (index < 0)
+	{
+		printf("랭킹에 데이터가 등록되어 있지 않습니다.");
+		return;
+	}
+	if (index >= *order) {
+		printf("잘못된 인덱스 값\n");
+		return;
+	}
+	for (int i = index; i < *order - 1; i++) { // 인덱스 번호를 선택하면 인덱스 번호 이하의 번호는 변화 x
+		rank[i] = rank[i + 1];
+
+	}
+	(*order)--; // 데이터를 삭제했기 때문에 현재 데이터의 총 갯수를 -1
+
+}
+
+int FindMaxIndex(Rank rank[], int size)
+{
+	int index = 0;
+	// Rank 데이터 안에서 정수가 가장 큰 인덱스를 찾아서 Index 변수에 저장하고 반환한다.
+	//if (rank[0].score > rank[1].score); // 0인덱스 값이 더 크면?
+	//if (rank[0].score > rank[2].score); // 임시로 저장할 변수가 필요하다.
+
+	int maxValue = rank[0].score; // 처음 데이터를 읽어왔을 때 가장 큰수는 당연히 처음의 데이터
+
+	for (int i = 1; i < size; i++) {
+		if (rank[i].score > maxValue) {
+			maxValue = rank[i].score;
+			index = i;
+		}
+	}
+	
+	return index;
 }
--- Ch12/main.c
+++ Ch12/main.c
@@ -7,42 +7,68 @@
 */
 
 #include "Rank.h"
+#include <stdlib.h>
+#include <conio.h>
 
 int main() {
-	printf("1. 파일을 저장하는 함수\n");
-	//FileSaveTemp();
-	printf("2. 파일을 읽어오는 함수\n");
-	FileLoadTemp();
-	printf("3. 랭크 구조체를 구현\n");
 
 	Rank ranks[MAXPLAYER];
 	int order = 0;
-	AddRank(ranks, &order, "Bear", 100);
+
+	/*AddRank(ranks, &order, "Bear", 100);
 	AddRank(ranks, &order, "Candy", 150);
 	AddRank(ranks, &order, "EldRich", 2500);
 	AddRank(ranks, &order, "BlueEyes", 3000);
 	AddRank(ranks, &order, "StarDust", 2500);
 	AddRank(ranks, &order, "Demonsmith", 3300);
-	AddRank(ranks, &order, "Earth-Machine", 3000);
+	AddRank(ranks, &order, "Earth-Machine", 3000);*/
 
+	// 1. 데이터 파일을 로드하는 화면 구성
 
-	order = LoadRank(RANKFILEPATH, ranks);
-
-	if (order < MAXPLAYER) {
-		printf("새로운 플레이어의 이름을 입력하세요 : ");
-		char newName[MAXLENGTH];
-		int newScore;
-
-		scanf("%49s", newName);
-		printf("점수를 입력하세요 : ");
-		scanf("%d", &newScore);
-
-		AddRank(ranks, &order, newName, newScore);
+	printf("데이터를 불러오는 중입니다\n");
+	for (int i = 0; i < 10; i++) {
+		printf("■");
+		Sleep(50);
 	}
 
-	PrintRanking(ranks, order);
+	printf("■■■■■■■■■■■\n");
+	Sleep(1000);
 
-	FileSave(RANKFILEPATH, ranks, order);
+	order = LoadRank(RANKFILEPATH, ranks);
+	printf("로딩이 완료되었습니다. 키를 입력하여 주세요.\n");
+	_getch(); // 키보드 문자 입력을 받는 함수
+	system("cls");
+
+	int firstRank = 0;
+
+	while (1) 
+	{
+		printf("1_랭킹 추가 2_랭킹 확인 3_프로그램 종료 4_랭킹 데이터 삭제 5_1위의 정보를 출력 : ");
+		int input = 0;
+		scanf("%d", &input);
+		int index = 0;
+		switch (input) 
+		{
+		case 1: AddRankData(ranks, &order); printf("랭킹 추가가 완료되었습니다.");break;
+		case 2: PrintRanking(ranks, order); break;
+		case 3: FileSave(RANKFILEPATH, ranks, order); return;
+		case 4:	
+			printf("삭제할 번호를 입력해주세요: ");
+			scanf("%d", &index);
+			index--; // index 번호를 -- 해주면 순서 번호와 같아진다.
+			DeleteRankData(ranks, &order,index);
+			printf("\n");
+			break;
+		case 5: 
+			firstRank = FindMaxIndex(ranks, order);
+			printf("랭킹 1위의 이름 : %s, 점수 : %d\n", ranks[firstRank].name, ranks[firstRank].score);
+			break;
+		default: printf("잘못된 입력. 다시 키를 입력하십시오\n");
+			_getch();
+			system("cls");
+			break;
+		}
+	}
 
 	
 }
